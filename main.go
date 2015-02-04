718119253a72c877235046bc434c8fde49da7532
Maxime Heckel <heckelmaxime@gmail.com>
2015-02-04 15:16:55 +0100

    new program extension for tutorial

--- main.go
+++ main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"runtime"
+
+	"gopkg.in/mgo.v2"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>hello, world</h1>\nI'm running on %s with an %s CPU ", runtime.GOOS, runtime.GOARCH)
+	fmt.Fprintf(w, "<h1>hello, world</h1>\nI'm running on %s with an %s CPU the name of this program is : %s", runtime.GOOS, runtime.GOARCH, os.Getenv("NAME"))
 }
 
 func main() {
+	session, err := mgo.Dial(os.Getenv("MONGO"))
+	if err != nil {
+		fmt.Println("No mongo db")
+	}
+	defer session.Close()
 	http.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":80", nil)
-  fmt.Println("running")
 }
