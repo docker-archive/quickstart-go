49ace65e56b80c3c853b6dcfb2dde1423fa8db7f
Borja Burgos <borja@tutum.co>
2015-02-06 20:00:08 -0500

    fixed spacing + added initial log line

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func mongoConnect() (s string) {
 	} else {
 		s = "Not available"
 	}
-		return s
+	return s
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -27,11 +27,10 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	mongostatus := mongoConnect()
 	fmt.Fprintf(w, "<h1>hello, %s</h1>\n<b>Hostname: </b>%s<br><b>MongoDB Status: </b>%s", os.Getenv("NAME"), hostname, mongostatus)
 	fmt.Println(hostname, "handled HTTP REQUEST at", time.Now(), "\nMongoDB Status:", mongostatus)
-
 }
 
 func main() {
-
 	http.HandleFunc("/", indexHandler)
+	fmt.Println("Listening on port 80 for requests...")
 	http.ListenAndServe(":80", nil)
 }
